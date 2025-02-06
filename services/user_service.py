29738191eff534fda29f0af3c6d4bc83a494dd61
Bthcorn <bowornthat.maize@gmail.com>
2025-02-06 18:36:01 +0700

    fix: using status for exception code handler

--- services/user_service.py
+++ services/user_service.py
@@ -3,7 +3,7 @@ from repositories.user_repository import (
     UserRepository,
     get_user_repository,
 )
-from fastapi import HTTPException, Depends
+from fastapi import HTTPException, Depends, status
 
 
 class UserService:
@@ -13,7 +13,9 @@ class UserService:
     def create_user(self, user_create: UserCreate) -> UserResponse:
         user = self.user_repository.find_by_username(user_create.username)
         if user:
-            raise HTTPException(status_code=400, detail="User already exists")
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST, detail="User already exists"
+            )
 
         return self.user_repository.create(user_create)
 
